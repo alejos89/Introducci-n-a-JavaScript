62583ea2c69cfedb64ea5401b703b58510deb2a9
alejos <aggomez@scl.edu.gt>
2025-07-08 10:10:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+console.log("[Variables y tipos de datos]");
+
+
+//Variables
+
+//Como declarar variables de tipo entero
+
+//Forma 1 (Antigua, no recomendable de usar)
+//var edad=10;
+//var estado=true;
+//var nombre="Thomas";
+
+
+//Forma 2 
+let edad=10;
+let estado=true;
+let nombre="Thomas";
+
+
+//Para que algo sea una constante
+//Las constantes deben escribirse con mayúsculas
+const PI=3.1416;
+
+
+
+console.log(edad,nombre, estado,PI);
+
+
+
+
+
+
+
+
+
+
